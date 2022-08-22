be5a990ce78ad067946b4783e4ba3c1aa2397dfe
Saiprasad8341 <saiprasad.8341@gmail.com>
2022-08-22 14:51:21 +0530

    secound commit

--- server.js
+++ server.js
@@ -29,8 +29,9 @@ const start = async () => {
     console.log(error);
   }
 };
-
+//server production assets
 if (process.env.NODE_ENV === "production") {
+  //app.use(express.static(path.join("client/build")))
   app.use("/", express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client/build/index.html"));
@@ -38,3 +39,7 @@ if (process.env.NODE_ENV === "production") {
 }
 app.get("/", (req, res) => res.send("Hello World..!"));
 start();
+
+//jsonfile
+// "dev": "concurrently \"npm run server\" \"npm run client\"",
+    // "start": "node server.js",
